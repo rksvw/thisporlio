305cfcef9476757c1b05666d936bb9b3353add9c
rksvw <ritik.vw98@gmail.com>
2025-02-20 18:45:51 +0530

    encrypted the users password.

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,10 +1,15 @@
 const {db} = require('../db/sql');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 // Create a new user
 async function signup(req, res) {
     console.log(req.body);
-    const {firstName, lastName, email, password ,username} = req.body;
-    db.query('INSERT INTO usersdata (firstName, lastName, email, password, username) VALUE (?, ?, ? , ? ,? )', [firstName, lastName, email, password, username], (err, result) => {
+    const password = req.body.password;
+    const encryptedPassword = await bcrypt.hash(password, saltRounds);
+
+    const {firstName, lastName, email, username} = req.body;
+    db.query('INSERT INTO usersdata (firstName, lastName, email, password, username) VALUE (?, ?, ? , ? ,? )', [firstName, lastName, email, encryptedPassword, username], (err, result) => {
         if (err){
             console.log('Error executing query: ', err.stack);
             res.status(400).send('Error creating user');
@@ -12,6 +17,14 @@ async function signup(req, res) {
         }
         res.status(201).send('User created successfully');
     })
+}
+
+async function signin(req, res) {
+    console.log(req.body);
+    if (req.body.email && req.body.password) {
+        const {email, password} = req.body;
+        db.query('')
+    }
 }
 
 module.exports = {
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,3 +2,6 @@ const router = require('express').Router();
 const {signup} = require('../controllers/user.controller');
 
 module.exports = router.post('/signup', signup);
+// module.exports = router.get('/about', rAbuot);
+// module.exports = router.get('/', rHome);
+// module.exports = router.post('/signin', signin)
