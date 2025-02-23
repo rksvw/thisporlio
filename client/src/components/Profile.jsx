6ade2c6eb605923c2923c8cb8c83d77c5e2c8451
rksvw <ritik.vw98@gmail.com>
2025-02-23 19:36:58 +0530

    completed the profile page banner and user profile picture and some input fields style.

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,15 +1,28 @@
 import React, { useRef, useState } from "react";
 
 function Profile() {
-  const [imageFile, setImageFile] = useState(null);
-  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [bgimageFile, setBgImageFile] = useState(null);
+  const [bgimageFileUrl, setBgImageFileUrl] = useState(null);
+  const [uImageFile, setUImageFile] = useState(null);
+  const [uImageFileUrl, setUImageFileUrl] = useState(null);
   const filePickerRef = useRef();
+  const imagePickerRef = useRef();
+  const defaultImage = "https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg";
+  const defaultBg = "https://wikitravel.org/upload/shared//6/6a/Default_Banner.jpg";
+
   const handleUserBgImage = (e) => {
     const file = e.target.files[0];
     if (file) {
-      setImageFile(file);
-      console.log(URL.createObjectURL(file));
-      setImageFileUrl(URL.createObjectURL(file));
+      setBgImageFile(file);
+      setBgImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+
+  const handleUserImage = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setUImageFile(file);
+      setUImageFileUrl(URL.createObjectURL(file));
     }
   };
 
@@ -17,8 +30,8 @@ function Profile() {
 
   return (
     <>
-      <div className="bg-amber-50 mx-auto p-3 w-lg">
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+      <div className="bg-amber-50 mx-auto p-3 w-lg rounded-2xl">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2 relative">
           <input
             type="file"
             id="inputImage"
@@ -27,10 +40,56 @@ function Profile() {
             ref={filePickerRef}
             hidden
           />
-          <div className="relative w-lg h-36 self-center cursor-pointer overflow-hidden" onClick={() => filePickerRef.current.click()}>
-            <img src={imageFileUrl} alt="user" className="w-full absolute h-full object-cover" />
+          <div
+            className="relative w-lg h-44 self-center cursor-pointer overflow-hidden rounded-tl-2xl rounded-tr-2xl bgImgP"
+            onClick={() => filePickerRef.current.click()}
+          >
+            <img
+              src={bgimageFileUrl || defaultBg}
+              alt="user"
+              className="w-full absolute h-full object-cover"
+            />
           </div>
-          {/* <input type="text" id="username" placeholder="username" defaultValue={"hello world"}  /> */}
+          <input
+            type="file"
+            name="userImage"
+            accept="image/*"
+            onChange={handleUserImage}
+            ref={imagePickerRef}
+            hidden
+          />
+          <div
+            className="absolute w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full top-30"
+            onClick={() => imagePickerRef.current.click()}
+          >
+            <img
+              src={uImageFileUrl || defaultImage}
+              alt=""
+              className="rounded-full w-full h-full object-cover border-4 border-[lightgray]"
+            />
+          </div>
+          <input
+            type="text"
+            id="username"
+            placeholder="username"
+            className="w-sm self-center ldvioclr rounded-lg mt-1.5 backdrop-blur-2xl bg-gradient-to-r from-purple-400 via-pink-600 to-indigo-500"
+            defaultValue={"hello world"}
+          />
+          <input
+            type="email"
+            id="email"
+            placeholder="email"
+            className="w-sm self-center ldvioclr rounded-lg mt-1.5 bg-gradient-to-r from-purple-400 via-pink-600 to-indigo-500"
+            defaultValue={"myemail@gmail.com"}
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className="w-sm self-center ldvioclr rounded-lg mt-1.5 bg-gradient-to-r from-purple-400 via-pink-600 to-indigo-500 "
+            placeholder="password"
+          />
+          <button type="submit" className="w-sm self-center h-10">Update</button>
         </form>
       </div>
     </>
